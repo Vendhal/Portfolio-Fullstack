cf62d1b094c98e5cd22edd64dddf14d0689e4fdb
Vendhal <2300033147@kluniversity.in>
2025-09-23 14:57:44 +0530

    Switch database stack to PostgreSQL

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,6 @@
 -- Optional: create schema objects or set defaults.
 -- With JPA ddl-auto=update, tables will be created automatically.
--- You can use this file to create users or seed data if desired.
+-- You can use this file to create extensions or seed data if desired.
 
 -- Example: ensure timezone
-SET GLOBAL time_zone = "+00:00";
+SET TIME ZONE 'UTC';
